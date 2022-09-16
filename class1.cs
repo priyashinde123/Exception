63db87817137377489c4c8f14830dc880f75b210
priyashinde123 <113039144+priyashinde123@users.noreply.github.com>
2022-09-16 15:15:30 +0530

    Add files via upload

---
+++ class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExeceptionHandling
+{
+    internal class class1
+    {
+        public static void execute()
+        {
+
+
+            //
+            /*
+
+           
+            Console.WriteLine(100/0);
+            */
+
+            //2
+            /*
+            int[] arr=new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                arr[i+1]=i;
+            }*/
+
+            //3
+            /*
+            class2 c;
+            Console.WriteLine(c.name);
+            */
+
+            //4
+            /*
+            float num = 30.15f;
+            int num1 = num;
+            */
+
+
+            String[] arr = File.ReadAllLines(path);
+        }
+    }
+    class class2
+    {
+      public  int id;
+       public int name;
+    }
+}
